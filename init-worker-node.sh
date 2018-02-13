3a6237040913afba1191c5298f0866b99fd8fbcb
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-02-13 13:36:25 -0600

    fix typo in init-worker-node.sh

--- init-worker-node.sh
+++ init-worker-node.sh
@@ -10,7 +10,7 @@ while ! test -f "$pool/pool_is_ready"; do
 	sleep 5
 done
 
-source $(dirname "${BASH_SOURCE[0]}")/condor-common.sh
+source $(dirname "${BASH_SOURCE[0]}")/common.sh
 condor_setup_common $pool
 export _CONDOR_STARTD_HISTORY = $_CONDOR_LOCAL_DIR/startd_history
 export _CONDOR_DAEMON_LIST="MASTER STARTD"
