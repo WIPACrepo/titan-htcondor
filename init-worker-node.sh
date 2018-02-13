36bfef21983e69e9fd64b18685019318586cb036
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-02-13 13:51:31 -0600

    fix typo in init-worker-node.sh

--- init-worker-node.sh
+++ init-worker-node.sh
@@ -12,7 +12,7 @@ done
 
 source $(dirname "${BASH_SOURCE[0]}")/common.sh
 condor_setup_common $pool
-export _CONDOR_STARTD_HISTORY = $_CONDOR_LOCAL_DIR/startd_history
+export _CONDOR_STARTD_HISTORY=$_CONDOR_LOCAL_DIR/startd_history
 export _CONDOR_DAEMON_LIST="MASTER STARTD"
 export | grep -v 'SSH\|PWD\|SHLVL' > /tmp/env
 dstat -t --all -p --proc-count -l --mem --swap >> $_CONDOR_LOCAL_DIR/dstat &
