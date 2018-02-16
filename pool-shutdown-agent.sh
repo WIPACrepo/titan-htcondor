601168253809ada1e01e177fa5afa094b7385d5e
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-02-16 11:39:13 -0600

    temporarily simplify pool-shutdown-agent.sh

--- pool-shutdown-agent.sh
+++ pool-shutdown-agent.sh
@@ -2,19 +2,28 @@
 # create pool_kill file on certain conditions
 
 pool=$1
-out_of_jobs_shutdown_delay=600
+#out_of_jobs_shutdown_delay=720
 source /tmp/env
 
 while true; do
 	sleep 10
 	idle_jobs=$(condor_status -sched -af TotalIdleJobs)
-	running_jobs=$(condor_status -sched -af TotalRunningJobs)
+	#dyn_slots=$(condor_status -con DynamicSlot | wc -l)
+	#part_slots=$(condor_status -con PartitionableSlot|wc -l)
 	if ((idle_jobs == 0)); then
 		echo idle_jobs=$idle_jobs running_jobs=$running_jobs
-		if ((running_jobs > 0)); then
-			sleep $out_of_jobs_shutdown_delay
-		fi
-		touch $pool/pool_kill
-		exit
+		while true; do
+			sleep 10
+			running_jobs=$(condor_status -sched -af TotalRunningJobs)
+			if ((running_jobs == 0)); then
+				touch $pool/pool_kill
+				exit
+			fi
+		done
+#		if ((running_jobs > 0)); then
+#			sleep $out_of_jobs_shutdown_delay
+#		fi
+#		touch $pool/pool_kill
+#		exit
 	fi
 done
