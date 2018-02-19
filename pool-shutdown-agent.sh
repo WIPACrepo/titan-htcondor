b0b1c45f7fda08b4b32e63c8bbf8235d0a7d48c7
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-02-19 14:08:01 -0600

    switch to timeout in pool-shutdown-agent.sh

--- pool-shutdown-agent.sh
+++ pool-shutdown-agent.sh
@@ -2,7 +2,7 @@
 # create pool_kill file on certain conditions
 
 pool=$1
-#out_of_jobs_shutdown_delay=720
+out_of_jobs_shutdown_delay=300
 source /tmp/env
 
 while true; do
@@ -11,19 +11,11 @@ while true; do
 	#dyn_slots=$(condor_status -con DynamicSlot | wc -l)
 	#part_slots=$(condor_status -con PartitionableSlot|wc -l)
 	if ((idle_jobs == 0)); then
-		echo idle_jobs=$idle_jobs running_jobs=$running_jobs
-		while true; do
-			sleep 10
-			running_jobs=$(condor_status -sched -af TotalRunningJobs)
-			if ((running_jobs == 0)); then
-				touch $pool/pool_kill
-				exit
-			fi
-		done
-#		if ((running_jobs > 0)); then
-#			sleep $out_of_jobs_shutdown_delay
-#		fi
-#		touch $pool/pool_kill
-#		exit
+		running_jobs=$(condor_status -sched -af TotalRunningJobs)
+		if ((running_jobs > 0)); then
+			sleep $out_of_jobs_shutdown_delay
+		fi
+		touch $pool/pool_kill
+		exit
 	fi
 done
