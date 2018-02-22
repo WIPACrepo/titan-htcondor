e58fbd446f021575ce173000fee114d784bc5bd0
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-02-22 12:44:51 -0600

    pool-shutdown-agent.sh now indicates when we are out of jobs

--- pool-shutdown-agent.sh
+++ pool-shutdown-agent.sh
@@ -11,6 +11,7 @@ while ! test -f "$pool/pool_kill"; do
 	#dyn_slots=$(condor_status -con DynamicSlot | wc -l)
 	#part_slots=$(condor_status -con PartitionableSlot|wc -l)
 	if ((idle_jobs == 0)); then
+		touch $pool/pool_no_idle_jobs
 		running_jobs=$(condor_status -sched -af TotalRunningJobs)
 		if ((running_jobs > 0)); then
 			sleep $out_of_jobs_shutdown_delay
