42623cc7cd6ca7892654b98f4ba97eef6ee35082
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-02-26 14:03:09 -0600

    add nmap check to init-worker-node.sh

--- init-worker-node.sh
+++ init-worker-node.sh
@@ -19,6 +19,8 @@ export | grep -v 'SSH\|PWD\|SHLVL' > /tmp/env
 dstat -t --all -p --proc-count -l --mem --swap >> $_CONDOR_LOCAL_DIR/dstat &
 nvidia-smi dmon -o DT -s um >> $_CONDOR_LOCAL_DIR/dmon &
 for ((;;)); do dmesg > $_CONDOR_LOCAL_DIR/dmesg; sleep 10 ; done &
+# ping fails with permission error, so use nmap
+for ((;;)); do date; nmap -sn $(<$pool/cm_addr); sleep 10 ; done >> $_CONDOR_LOCAL_DIR/nmap &
 shutdown_on_pool_kill $pool &
 
 condor_master -f 
