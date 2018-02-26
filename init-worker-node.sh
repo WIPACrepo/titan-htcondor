2645a32b1afb146591c3d10c639c9f2497023bf3
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-02-26 13:39:05 -0600

    add ping check to init-worker-node.sh

--- init-worker-node.sh
+++ init-worker-node.sh
@@ -9,6 +9,7 @@ while ! test -f "$pool/pool_is_ready"; do
 	echo Waiting for $pool/pool_is_ready
 	sleep 5
 done
+echo Starting condor on $(hostname)
 
 source $(dirname "${BASH_SOURCE[0]}")/common.sh
 condor_setup_common $pool
@@ -17,6 +18,8 @@ export _CONDOR_DAEMON_LIST="MASTER STARTD"
 export | grep -v 'SSH\|PWD\|SHLVL' > /tmp/env
 dstat -t --all -p --proc-count -l --mem --swap >> $_CONDOR_LOCAL_DIR/dstat &
 nvidia-smi dmon -o DT -s um >> $_CONDOR_LOCAL_DIR/dmon &
+ping -i 10 $(<$pool/cm_addr) | awk '{print strftime("[%Y-%m-%d %H:%M:%S]"), $0}' \
+														>> $_CONDOR_LOCAL_DIR/ping &
 shutdown_on_pool_kill $pool &
 
 condor_master -f 
