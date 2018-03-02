43f4391d3bc67f8624e96154191e98fdabe6f6df
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-03-02 12:48:45 -0600

    implement pool_nodes

--- common.sh
+++ common.sh
@@ -4,7 +4,9 @@ function condor_setup_common() {
 	local pool=$1
 	export _CONDOR_LOCAL_DIR=$pool/nodes/$(hostname)
 	mkdir -p $_CONDOR_LOCAL_DIR
-	export _CONDOR_NETWORK_INTERFACE="$(ip -4 addr show dev ipogif0 | awk -F '[ /]+' '/inet/{print $3}')"
+	local ipaddr="$(ip -4 addr show dev ipogif0 | awk -F '[ /]+' '/inet/{print $3}')"
+	echo $ipaddr >> $pool/pool_nodes
+	export _CONDOR_NETWORK_INTERFACE=$ipaddr
 	export _CONDOR_LOCAL_CONFIG_DIR=$pool/config
 	export _CONDOR_EXECUTE=/tmp/execute
 	mkdir -p $_CONDOR_EXECUTE
