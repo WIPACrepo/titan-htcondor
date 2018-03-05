106b168568ab1325a2ba8e21ac1cfb37c7034119
Vladimir Brik <22751545+vbrik@users.noreply.github.com>
2018-03-05 16:30:53 -0600

    stop rotating event log and history

--- common.sh
+++ common.sh
@@ -36,6 +36,8 @@ function create_pool_config() {
 		RUN = \$(LOCAL_DIR)
 		LOCK = \$(LOCAL_DIR)
 		MAX_DEFAULT_LOG = 0
+		EVENT_LOG_MAX_ROTATIONS = 0
+		MAX_HISTORY_ROTATIONS = 0
 
 		RUNBENCHMARKS = False
 		use feature : GPUs
